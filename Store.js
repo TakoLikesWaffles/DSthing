ec66a62debc4e9745711ada11e11bc1cf6258621
Tako <118289382+TakoLikesWaffles@users.noreply.github.com>
2025-12-11 16:33:10 +0100

    Update Store.js

--- Store.js
+++ Store.js
@@ -1,1 +1,11 @@
-nothing here yet
+[
+  {
+    "id": "whiteboard",
+    "name": "Whiteboard",
+    "description": "A mini IbisPaint-inspired drawing application with layers, brushes, and gallery.",
+    "icon": "Icons/PaintIcon.png",
+    "folder": "Whiteboard",
+    "entry": "Whiteboard/Launcher.py",
+    "price": 0,
+    "type": "Software"
+  },
